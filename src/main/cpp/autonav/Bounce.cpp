a507cfa9a818c7c4066331621f184b5268bb2bbd
redstorm509 <redstorm509@gmail.com>
2021-03-26 18:19:35 -0400

    Bounce path almost done

--- src/main/cpp/autonav/Bounce.cpp
+++ src/main/cpp/autonav/Bounce.cpp
@@ -6,8 +6,36 @@ void Bounce::Run(){
 
     //  ms = time in milliseconds
     //  autoDrive( speed, rotation, reverse )
-    if(ms <= 1000){
-
+    if(ms <= 1350){
+        autoDrive(2000, 600, false);
+    } 
+    else if(ms <= 2800){
+        autoDrive(-2000, -100, false);
+    }
+    else if(ms <= 4700){
+        autoDrive(-2000, 1000, false);
+    }
+    else if(ms <= 6000){
+        autoDrive(-2000, 175, false);
+    }
+    else if(ms <= 7500){
+        autoDrive(2000, -175, false);
+    }
+    else if(ms <= 8000){
+        autoDrive(2000, 1600, false);
+    }
+    else if(ms <= 8550){
+        autoDrive(2000, -175, false);
+    }
+    else if(ms <= 9250){
+        autoDrive(2000, 1600, false);
+    }
+    else if(ms <= 11000){
+        autoDrive(2000, -175, false);
+    }
+    //  Needs work
+    else if(ms <= 12300){
+        autoDrive(-2000, 1000, false);
     }
     else {
         autoDrive(0.0f, 0.0f, false);
